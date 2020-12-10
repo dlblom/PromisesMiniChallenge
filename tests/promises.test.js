ae4cbd2e2093f4c7f87cafc248067379f9b00267
Danielle Blom <dlblom91@gmail.com>
2020-12-10 15:00:20 -0600

    Add question and test for promise states

--- tests/promises.test.js
+++ tests/promises.test.js
@@ -1,4 +1,7 @@
-const { tellEveryoneQuestion } = require("../src/promises");
+const {
+  tellEveryoneQuestion,
+  promiseStateQuestion,
+} = require("../src/promises");
 
 describe("tellEveryoneQuestion", () => {
   it("returns the console.log statements in the correct order", () => {
@@ -7,5 +10,14 @@ describe("tellEveryoneQuestion", () => {
       "after learning promises",
       "Hey friend, I learned promises",
     ]);
+  });
+});
+
+describe("promiseStateQuestion", () => {
+  it("returns an array containing the 3 states of promsies", () => {
+    expect(promiseStateQuestion()).toContain("PENDING");
+    expect(promiseStateQuestion()).toContain("FULFILED");
+    expect(promiseStateQuestion()).toContain("REJECTED");
+    expect(promiseStateQuestion()).toHaveLength(3);
   });
 });
